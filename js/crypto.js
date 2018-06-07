025ceff132d7617656da53a7ab9aaa28858e0b44
Brandon Betts and Renee Sarley <renee@>
2018-06-07 15:44:35 -0700

    add more scripts to crypto.js

--- js/crypto.js
+++ js/crypto.js
@@ -1,9 +1,27 @@
-function crypto(message){
+function crypto(message) {
  var noPunc = message.replace(/[.,\/#!$%\^&\*;:{}<>=\-_`'~()]/g,"")
+
  var noSpace = noPunc.replace(/\s+/g, '')
  var lowerCase = noSpace.toLowerCase()
- var charCount = lowerCase.length
+  alert(lowerCase);
+
+ var charArray = lowerCase.split('')
+ alert(charArray)
+
+ var charCount = charArray.length
+ alert(charCount)
+
  var squareRoot = Math.sqrt(charCount)
- return(squareRoot)
- for (i=0; i>)
+ alert(squareRoot)
+ var secretArray = []
+ for(var i = 0; i < charCount; i++) {
+    secretArray.push(" ");
+}
+  secretArray.splice(0, 1, (charArray[0]));
+  alert("the secret array is:" + secretArray)
+ for (i=1; i < charArray.length; i++) {
+   secretArray.splice(i, 1, charArray[i+squareRoot])
+ }
+    return secretArray
+ alert("past the loop")
 }
